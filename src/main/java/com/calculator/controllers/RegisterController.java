ac3b3bfcc81dc9c8776adc739f4af46cca3490fe
pap7tomek <pap7tomek@gmail.com>
2018-05-21 01:20:09 +0200

    rejestracja poczatek plus dashboard

--- src/main/java/com/calculator/controllers/RegisterController.java
+++ src/main/java/com/calculator/controllers/RegisterController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
@@ -16,5 +17,19 @@ public class RegisterController {
     @GetMapping("/login")
     public String login(Model model){
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(Model model){
+        return "index";
+    }
+    @GetMapping("/register")
+    public String register(Model model){
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(Model model){
+        return "dashbord";
     }
 }
