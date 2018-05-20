b92eb146c765dfd490ca9a361dd35eae7ff70017
pap7tomek <pap7tomek@gmail.com>
2018-05-21 01:12:40 +0200

    widok logowania

--- src/main/java/com/calculator/controllers/RegisterController.java
+++ src/main/java/com/calculator/controllers/RegisterController.java
@@ -1,13 +1,20 @@
 package com.calculator.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class RegisterController {
-    @Autowired
+
     @GetMapping("/")
     public String index(Model model){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        return "login";
     }
 }
