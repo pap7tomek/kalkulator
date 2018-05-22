80efb2fb96bc9d69d23d3da5cac1b66a46376ed0
pap7tomek <pap7tomek@gmail.com>
2018-05-23 00:51:41 +0200

    poprawienie autenticationmanagera

--- src/main/java/com/calculator/security/SecurityConfig.java
+++ src/main/java/com/calculator/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.calculator.security;
 import com.calculator.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +13,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
--- src/main/java/com/calculator/service/UserDetailsServiceImpl.java
+++ src/main/java/com/calculator/service/UserDetailsServiceImpl.java
@@ -17,6 +17,7 @@ import java.util.Set;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
     @Autowired
     private UserRepository userRepository;
 
