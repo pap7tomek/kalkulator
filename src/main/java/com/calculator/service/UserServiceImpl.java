e38a3af1908605b3a2f9f46c755a537b4707e300
pap7tomek <pap7tomek@gmail.com>
2018-05-23 00:59:44 +0200

    userService

---
+++ src/main/java/com/calculator/service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.calculator.service;
+
+import com.calculator.repository.RoleRepository;
+import com.calculator.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+}
